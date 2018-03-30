f76afd7330d360b58b90f400394472361e1815e1
gael243 <gmapwata6@yahoo.fr>
2018-03-30 13:51:35 +0200

    la nouvelle correction des differents remarque

--- Mars_2018/app.js
+++ Mars_2018/app.js
@@ -1,21 +1,14 @@
 /**
  * Created by GAELMP on 26/08/2017.
  */
-const express = require('express');
 
-
-let  fileSystem=require('fs');
-
+const  fileSystem=require('fs');
 const archiver=require('archiver');
-
 const output= fileSystem.createWriteStream('./x.zip');
-
 const archive=archiver('zip',{
     gzip:true,
     zlib:{level:9}
 });
-let app = express();
-
 output.on('close',function() => {
     `${archiver.pointer()}total bytes`;
 });
@@ -25,8 +18,3 @@ archive.file('./Etudiant/afterchild.txt',{name:'afterchild.txt'});
 archive.file('./Etudiant/commande.txt',{name:'commande.txt'});
 archive.on('error',(err)=>{throw err;});
 archive.finalize();
-app.use(express.static('client/'));
-app.listen(4000,() => {
-    console.log('le serveur tourne sur le port 4000');
-});
-
