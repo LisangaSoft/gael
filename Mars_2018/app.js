39448092efc59bea6d42ee8e0545dc42378062b4
gael243 <gmapwata6@yahoo.fr>
2018-03-30 13:27:28 +0200

    la correction des differents remarque

--- Mars_2018/app.js
+++ Mars_2018/app.js
@@ -2,38 +2,31 @@
  * Created by GAELMP on 26/08/2017.
  */
 const express = require('express');
-const bodyParser = require('body-parser');
 
-var  file_system=require('fs');
 
-var archiver=require('archiver');
+let  fileSystem=require('fs');
 
-var output= file_system.createWriteStream('./x.zip');
+const archiver=require('archiver');
 
-var archive=archiver('zip',{
+const output= fileSystem.createWriteStream('./x.zip');
+
+const archive=archiver('zip',{
     gzip:true,
     zlib:{level:9}
 });
-var app = express();
+let app = express();
 
-output.on('close',function(){
-    console.log(archive.pointer()+'total bytes');
-    console.log('archiver has bee finalized');
+output.on('close',function() => {
+    `${archiver.pointer()}total bytes`;
 });
 archive.pipe(output);
 archive.file('./Etudiant/achanger.txt',{name:'change.txt'});
 archive.file('./Etudiant/afterchild.txt',{name:'afterchild.txt'});
 archive.file('./Etudiant/commande.txt',{name:'commande.txt'});
-archive.on('error',function(err){
-    throw err;
-});
+archive.on('error',(err)=>{throw err;});
 archive.finalize();
-
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-
 app.use(express.static('client/'));
-app.listen(3000,() => {
-    console.log('le serveur tourne sur le port 3000');
+app.listen(4000,() => {
+    console.log('le serveur tourne sur le port 4000');
 });
 
